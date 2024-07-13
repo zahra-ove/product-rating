2c160873039e3eff9b387133ad5fb598f15bdda6
zahra_ove <zahra.oveyedzade@gmail.com>
2024-07-13 13:43:51 +0330

    Modify api route file

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('products', ProductController::class);
 
 //product-rating
-Route::get('products-rating/{product_id}/{product_attribute_id}', [ProductRatingController::class, 'show']);
-Route::get('products-rating/{product_id}', [ProductRatingController::class, 'index']);
-Route::post('products-rating', [ProductRatingController::class, 'store']);
-Route::patch('products-rating', [ProductRatingController::class, 'update']);
-Route::delete('products-rating/{product_rating_id}', [ProductRatingController::class, 'destroy']);
+Route::get('products-rating/{product_id}', [ProductRatingController::class, 'index'])->name('products-rating.index');
+Route::get('products-rating/{product_id}/{product_attribute_id}', [ProductRatingController::class, 'show'])->name('products-rating.show');
+Route::post('products-rating', [ProductRatingController::class, 'store'])->name('products-rating.store');
+Route::patch('products-rating', [ProductRatingController::class, 'update'])->name('products-rating.update');
+Route::delete('products-rating/{product_rating_id}', [ProductRatingController::class, 'destroy'])->name('products-rating.destroy');
